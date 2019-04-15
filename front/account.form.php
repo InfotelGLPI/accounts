5faeaf74c898918c50cefa7f4891638358329180
keha35 <kevin.hainry@infotel.com>
2019-04-15 12:24:53 +0200

    Update Security flaw for 9.4

--- front/account.form.php
+++ front/account.form.php
@@ -116,7 +116,40 @@ if (isset($_POST["add"])) {
       Html::helpHeader(PluginAccountsAccount::getTypeName(2));
    }
 
-   $account->display(['id' => $_GET['id']]);
+   $account->check($_GET['id'], READ);
+   if(isset($_GET['id'])
+       && $_GET['id'] != 0
+       && !Session::haveRight("plugin_accounts_see_all_users", 1)){
+      $access = 0;
+      if($account->fields["groups_id"] == 0 &&
+          $account->fields["users_id"] == 0){
+         $access = 1;
+      }
+      if (Session::haveRight("plugin_accounts_my_groups", 1)) {
+         if ($account->fields["groups_id"]) {
+            if (count($_SESSION['glpigroups'])
+                && in_array($account->fields["groups_id"], $_SESSION['glpigroups'])) {
+               $access = 1;
+            }
+         }
+         if ($account->fields["users_id"]) {
+            if ($account->fields["users_id"] == Session::getLoginUserID())
+               $access = 1;
+         }
+      }
+      if (!Session::haveRight("plugin_accounts_my_groups", 1)
+          && $account->fields["users_id"] == Session::getLoginUserID()){
+         $access = 1;
+      }
+
+      if ($access != 1){
+         Html::displayRightError();
+      } else{
+         $account->display(['id' => $_GET['id']]);
+      }
+   } else{
+      $account->display(['id' => $_GET['id']]);
+   }
 
    if (Session::getCurrentInterface() == 'central') {
       Html::footer();
