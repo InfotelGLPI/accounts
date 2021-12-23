b468c408c20ba622ea443a351d0487924930e494
tsmr <thetsmr@gmail.com>
2021-12-23 09:12:16 +0100

    Prepare new RC

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_ACCOUNTS_VERSION', '3.0.0-rc1');
+define('PLUGIN_ACCOUNTS_VERSION', '3.0.0-rc2');
 
 if (!defined("PLUGIN_ACCOUNTS_DIR")) {
    define("PLUGIN_ACCOUNTS_DIR", Plugin::getPhpDir("accounts"));
