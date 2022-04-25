838ba064d17f79baddda47de7c30601a40435a58
tsmr <thetsmr@gmail.com>
2022-04-25 08:46:03 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_ACCOUNTS_VERSION', '3.0.0-rc2');
+define('PLUGIN_ACCOUNTS_VERSION', '3.0.0');
 
 if (!defined("PLUGIN_ACCOUNTS_DIR")) {
    define("PLUGIN_ACCOUNTS_DIR", Plugin::getPhpDir("accounts"));
