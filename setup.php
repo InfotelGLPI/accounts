59480752dc0eb864e94334418707997fda7a6992
Thetsmr <thetsmr@gmail.com>
2026-02-27 17:14:36 +0100

    Bump version 3.1.7

--- setup.php
+++ setup.php
@@ -37,7 +37,7 @@ use GlpiPlugin\Accounts\Config;
 use GlpiPlugin\Accounts\Profile;
 use GlpiPlugin\Accounts\Servicecatalog;
 
-define('PLUGIN_ACCOUNTS_VERSION', '3.1.6');
+define('PLUGIN_ACCOUNTS_VERSION', '3.1.7');
 
 if (!defined("PLUGIN_ACCOUNTS_DIR")) {
     define("PLUGIN_ACCOUNTS_DIR", Plugin::getPhpDir("accounts"));
