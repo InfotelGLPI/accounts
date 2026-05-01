bad9cc990033e1a4cd72c87dc4fad4b0e9b032a7
Richard Loureiro <richard.infoo@gmail.com>
2026-05-01 17:37:30 -0300

    fix: add TOTP decrypt to the actual JS file (account.form.js.php)
    The plugin loads account.form.js.php (PHP wrapper), not account.form.js.
    Add decrypt_totp_secret() to the correct file and remove the unused one.

--- public/account.form.js.php
+++ public/account.form.js.php
@@ -15,6 +15,7 @@ var auto_decrypt = function (suffix) {
        $("#hidden_password" + suffix).val($("#wrong_key_locale").val());
    } else {
        decrypt_password(root_accounts_doc, suffix);
+       decrypt_totp_secret(suffix);
    }
 };
 
@@ -26,6 +27,27 @@ var uncryptpassword = async function(suffix) {
     } else {
         document.getElementById('wrong_key_locale_div').innerHTML = '';
         await decrypt_password(root_accounts_doc, suffix);
+        decrypt_totp_secret(suffix);
+    }
+};
+
+var decrypt_totp_secret = function (suffix) {
+    suffix = suffix || "";
+    var aeskey = $("#aeskey" + suffix).val();
+    var encrypted = $("#encrypted_totp_secret" + suffix).val();
+    if (!encrypted || !aeskey) return;
+
+    var decrypted = '';
+    if (encrypted.startsWith('$v2$')) {
+        decrypted = decryptV2(encrypted, aeskey);
+    } else if (typeof AESDecryptCtr === 'function') {
+        decrypted = AESDecryptCtr(encrypted, SHA256(aeskey), 256);
+    } else {
+        decrypted = encrypted;
+    }
+
+    if ($("#hidden_totp_secret" + suffix).length) {
+        $("#hidden_totp_secret" + suffix).val(decrypted);
     }
 };
 
